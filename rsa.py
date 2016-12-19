ed16a44024e62968bb814d3800e13b08ef7d343a
Joseph E.S. Cieslik <unknownmasayoshi@gmail.com>
2016-12-18 23:05:15 -0500

    Added isPrime and filled out encrypt and decrypt, isPrime tests passing

--- rsa.py
+++ rsa.py
@@ -1,17 +1,39 @@
-def generatePrimes(length):
+def isPrime(prime):
+	"""Checks if a number is prime"""
+	if prime <= 1:
+		return False
+	elif prime <= 3:
+		return True
+	elif prime % 2 == 0 or prime % 3 == 0:
+		return False
+	base = 5
+	while base * base <= prime:
+		if prime % base == 0 or prime % (base + 2) == 0:
+			return False
+		base += 6
+	return True
+
+def generatePrimes(min, max):
 	"""Generates primes of a certain length"""
+	pass
 
 def keyGen():
 	"""Generates prime numbers and uses them to create a public private key pair"""
+	pass
 
-def encrypt():
+def encrypt(plaintext, public, mod):
 	"""Encrypts a message using a public key"""
+	final = [(ord(x) ** public) % mod for x in plaintext] #because pow() does not support bytes
+	return final
 
-def decrypt():
+def decrypt(ciphertext, private, mod):
 	"""Decrypts a message using a private key"""
+	final = [chr((x ** private) % mod) for x in ciphertext]
+	return final
 
 def main():
 	"""Debugging rsa functions"""
+	pass
 
 if __name__ == "__main__":
 	main()
--- tests.py
+++ tests.py
@@ -1,15 +1,24 @@
 import unittest
 import rsa
 
-def isPrime(prime):
-	"""Checks if a number is prime"""
-
 class RSATests(unittest.TestCase):
 	"""Tests for rsa.py"""
 
-	def primeTest(self):
-		"""Determine if numbers produced by generatePrimes function are prime"""
-		return self.assertTrue(isPrime(rsa.generatePrimes(5)))
+	def testIsPrime(self):
+		"""Check isPrime function with primes"""
+		self.assertTrue(rsa.isPrime(2) and rsa.isPrime(3) and rsa.isPrime(5) and rsa.isPrime(11) and rsa.isPrime(15485863))
 
+	def testIsNotPrime(self):
+		"""Check isPrime function with non prime numbers"""
+		self.assertFalse(rsa.isPrime(8) and rsa.isPrime(200))
+'''
+	def testEncrypt(self):
+		"""Checks output of encrypt function"""
+		self.assertTrue(rsa.encrypt("65", 17, 3233) == 2790)
+
+	def testDecrypt(self):
+		"""Checks output of decrypt function"""
+		self.assertTrue(rsa.decrypt(2790, 2753, 3233) == 65)
+'''
 if __name__ == "__main__":
 	unittest.main()
